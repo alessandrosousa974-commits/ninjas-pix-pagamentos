ad496ae6b288eb98f881fa30dd31b4e782393378
alessandrosousa974-commits <alessandrosousa974@gmail.com>
2025-12-29 09:25:16 -0300

    Create firebaseConfig.js

---
+++ public/public/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Substitua os valores abaixo pelas configurações do seu Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO_ID",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SUA_APP_ID"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
